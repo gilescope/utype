ad92c99fb005ab3bd2632a31eaf1236f9dade2e5
Giles Cope <gilescope@gmail.com>
2023-04-08 22:16:55 +0100

    window view

--- src/main.rs
+++ src/main.rs
@@ -1,165 +1,277 @@
+#![feature(associated_type_bounds)]
+use std::fs::File;
+use std::io::Read;
 use std::io::{stdin, stdout, Write};
 use std::time::Instant;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-
 mod model;
 
 use model::*;
 
+const HEADER: u16 = 5;
+
+fn draw_screen(page: &Page, cursor: &Cursor, copy: &Page, clear: bool) {
+    if clear {
+        print!("{}", termion::clear::All)
+    };
+    print!(
+        r#"{}{}"#,
+        termion::cursor::Goto(1, HEADER),
+        &termion::color::Fg(termion::color::LightBlue)
+    );
+
+    for (line_index, line) in page
+        .lines
+        .iter()
+        .enumerate()
+        .skip(cursor.window.start_line)
+        .take(cursor.window.size)
+    {
+        let mut is_first = true;
+        if let Some(copy_line) = copy.lines.get(line_index) {
+            for (word_index, word) in line.words.iter().enumerate() {
+                if is_first {
+                    is_first = false;
+                } else {
+                    print!(" ");
+                }
+
+                if let Some(copy_word) = copy_line.words.get(word_index) {
+                    for (index, ch) in word.chars.iter().enumerate() {
+                        if copy_word.chars.len() > index {
+                            if copy_word.chars[index] == *ch {
+                                print!("{}", termion::color::Fg(termion::color::Green));
+                            } else {
+                                print!("{}", termion::color::Fg(termion::color::Red));
+                            }
+                            print!("{}", copy_word.chars[index]);
+                            print!("{}", termion::color::Fg(termion::color::LightBlue));
+                        } else {
+                            print!("{ch}");
+                        }
+                    }
+                } else {
+                    print!("{}", word.chars.iter().collect::<String>());
+                }
+            }
+        } else {
+            for word in line.words.iter() {
+                if is_first {
+                    is_first = false;
+                } else {
+                    print!(" ");
+                }
+                print!("{}", word.chars.iter().collect::<String>());
+            }
+        }
+        print!("\r\n");
+    }
+}
+
+/// read or create a file with the current state of the game
+fn read_or_create(path: &str) -> String {
+    let mut file = File::options()
+        .create(true)
+        .read(true)
+        .append(true)
+        .open(path)
+        .unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    contents
+}
+
 /// Words per minute are defined as being 5 chars long (not including spaces - we're harsher here)
 fn main() {
+    let (_terminal_width, terminal_height) = termion::terminal_size().unwrap();
     let text = std::fs::read_to_string("/home/gilescope/git/type/war_of_the_worlds.txt").unwrap();
-    // let lines = text.lines();
+    let input_text = read_or_create("/home/gilescope/git/type/war_of_the_worlds.inputs.txt");
     let stdin = stdin();
     //setting up stdout and going into raw mode
     let mut stdout = stdout().into_raw_mode().unwrap();
 
-    //printing welcoming message, clearing the screen and going to left top corner with the cursor
+    //clear the screen and go to top left corner with the cursor
     print!(r#"{}{}"#, termion::cursor::Goto(1, 1), termion::clear::All);
+    let window = Window {
+        start_line: 0,
+        size: (terminal_height as usize).saturating_sub(HEADER as usize),
+    };
 
-    let page = Page::parse(&text);
-    let mut cursor = Cursor::new(&page);
-    print!(r#"{}"#, termion::cursor::Goto(1, 3));
-    for line in page.lines.iter().take(10) {
-        let mut is_first = true;
-        for word in line.words.iter() {
-            if is_first {
-                is_first = false;
-            } else {
-                print!(" ");
-            }
-            print!("{}", word.chars.iter().collect::<String>());
-        }
-        print!("\r\n");
-    }
+    let page = Page::parse(&text, false);
+    let mut copy = Page::parse("", true);
+    let mut cursor = Cursor::new(&page, window);
+    draw_screen(&page, &cursor, &copy, true);
 
-    print!(r#"{}"#, termion::cursor::Goto(1, 3));
+    print!(r#"{}"#, termion::cursor::Goto(1, HEADER));
 
     stdout.flush().unwrap();
 
-    let mut cumulative_chars_typed = 0;
-    let mut cumulative_time_taken: u128 = 0;
-
-    let mut target: Vec<char> = cursor.current_word().unwrap().chars.clone();
-    let mut trial: Vec<char> = vec![];
     let mut start: Option<Instant> = None;
 
-    let mut success: u32 = 0;
-    let mut failure: u32 = 0;
+    let mut analytics = Analytics {
+        cumulative_chars_typed: 0,
+        cumulative_time_taken: 0,
+        success: 0,
+        failure: 0,
+        percentage: 0.0,
+    };
 
-    let mut percentage: f32;
-
+    for mut c in input_text.chars() {
+        if c == '\n' {
+            c = ' '
+        }
+        process_key(
+            &page,
+            &mut copy,
+            Key::Char(c),
+            &mut stdout,
+            &mut cursor,
+            &mut analytics,
+            &mut start,
+        );
+    }
     for c in stdin.keys() {
-        match c.unwrap() {
-            Key::Ctrl('w') | Key::Ctrl('c') => {
-                stdout.suspend_raw_mode().unwrap();
-                std::process::exit(0)
-            }
-            Key::Char(' ') => {
-                print!(" ");
-                cursor.next_char();
-                if trial == target {
-                    cumulative_chars_typed += target.len();
+        process_key(
+            &page,
+            &mut copy,
+            c.unwrap(),
+            &mut stdout,
+            &mut cursor,
+            &mut analytics,
+            &mut start,
+        );
+    }
+}
 
-                    target = cursor
+struct Analytics {
+    cumulative_chars_typed: usize,
+    cumulative_time_taken: u128,
+    success: u32,
+    failure: u32,
+    percentage: f32,
+}
+
+fn process_key<T: Write>(
+    page: &Page,
+    copy: &mut Page,
+    c: Key,
+    stdout: &mut termion::raw::RawTerminal<T>,
+    cursor: &mut Cursor,
+    ctx: &mut Analytics,
+    start: &mut Option<Instant>,
+) {
+    let mut clear = false;
+    match c {
+        Key::Ctrl('c') => {
+            stdout.suspend_raw_mode().unwrap();
+            std::process::exit(0)
+        }
+        Key::Backspace | Key::Delete => {
+            copy.delete();
+            let current_word = cursor.word_number();
+            cursor.prev_char();
+
+            if cursor.word_number() != current_word {
+                ctx.cumulative_chars_typed = ctx.cumulative_chars_typed.saturating_sub(
+                    cursor
                         .current_word()
                         .unwrap_or(&Word { chars: vec![] })
                         .chars
-                        .clone();
-                    success += 1;
-                    if let Some(start) = start {
-                        let duration: u128 = (Instant::now() - start).as_millis();
-                        cumulative_time_taken += duration;
-                    }
-                } else {
-                    failure += 1;
-                }
-                start = None;
-                trial.clear();
-            }
-            Key::Backspace | Key::Delete => {
-                let current_word = cursor.word_number();
-                let current_char = cursor.char_number();
-                cursor.prev_char();
-
-                let c = cursor.current_char().unwrap_or(' ');
-                print!(
-                    "{}{}{}{}",
-                    termion::cursor::Left(1),
-                    &termion::color::Fg(termion::color::Blue),
-                    c,
-                    termion::cursor::Left(1)
+                        .len(),
                 );
-
-                if cursor.word_number() != current_word {
-                    cumulative_chars_typed = cumulative_chars_typed.saturating_sub(
-                        cursor
-                            .current_word()
-                            .unwrap_or(&Word { chars: vec![] })
-                            .chars
-                            .len(),
-                    );
-                }
-                if cursor.char_number() < current_char {
-                    if trial.len() > 0 {
-                        trial.pop();
-                    } else {
-                        target = cursor
-                            .current_word()
-                            .unwrap_or(&Word { chars: vec![] })
-                            .chars
-                            .clone();
-                        trial = target.clone();
-                    }
-                }
             }
-            Key::Char(c) => {
-                cursor.next_char();
-                if start == None {
-                    start = Some(Instant::now());
-                }
-                trial.push(c);
-                let same = trial.len() <= target.len() && target[trial.len() - 1] == c;
-
-                if same {
-                    print!("{}{}", &termion::color::Fg(termion::color::Green), c);
-                } else {
-                    print!("{}{}", &termion::color::Fg(termion::color::Red), c);
-                };
-            }
-            _ => {}
         }
+        Key::Char(c) => {
+            //if meant to be a space but we got it wrong put an _ in there.
+            let current_word = cursor.current_word_or_default().clone();
+            let last_word = copy.last_word_or_default();
+            if c == ' ' || last_word.chars.len() == current_word.chars.len() {
+                let current_word = cursor.current_word_or_default().clone();
+                let trial = copy.last_word_or_default().clone();
+                let is_short = trial.chars.len() < current_word.chars.len();
 
-        let wpm = if cumulative_time_taken == 0 {
-            0
-        } else {
-            ((cumulative_chars_typed as f32 / 5.) / (cumulative_time_taken as f32 / 60000.)) as u32
-        };
+                let new_line = cursor.next_char();
+                if new_line {
+                    clear = true;
+                }
+                let expected = *current_word.chars.get(trial.chars.len()).unwrap_or(&'X');
+                if is_short {
+                    let ascii_case_bit = 0b00100000;
+                    let is_alpha = expected.is_ascii_alphabetic();
+                    let c = expected as u32;
+                    let c = if is_alpha {
+                        char::from_u32(c ^ ascii_case_bit).unwrap()
+                    } else {
+                        'X'
+                    };
+                    // trial.push(c);
+                    copy.push(c, false);
+                } else {
+                    copy.push(' ', new_line);
+                }
+                if *trial.chars == current_word.chars {
+                    ctx.cumulative_chars_typed += current_word.chars.len();
 
-        percentage = if success == 0 {
-            0.
-        } else {
-            100. * ((success as f32) / ((success + failure) as f32))
-        };
+                    ctx.success += 1;
+                    if let Some(start) = start {
+                        let duration: u128 = (Instant::now() - *start).as_millis();
+                        ctx.cumulative_time_taken += duration;
+                    }
+                } else {
+                    ctx.failure += 1;
+                }
 
-        print!(
-            "{}{}{}:{}:{}     WPM:{} {:.2}%   {}",
-            termion::cursor::Save,
-            termion::cursor::Goto(1, 1),
-            cursor.line_number(),
-            cursor.word_number(),
-            trial.len(),
-            wpm,
-            percentage,
-            termion::cursor::Restore
-        );
+                if !is_short {
+                    *start = None;
+                }
+            } else {
+                cursor.next_char();
+                copy.push(c, false);
 
-        print!(
-            r#"{}"#,
-            termion::cursor::Goto(cursor.char_number(), cursor.line_number() as u16 + 2)
-        );
-        stdout.flush().unwrap();
+                if start.is_none() {
+                    *start = Some(Instant::now());
+                }
+            }
+        }
+        _ => {}
     }
+
+    draw_screen(page, cursor, copy, clear);
+    let wpm = if ctx.cumulative_time_taken == 0 {
+        0
+    } else {
+        ((ctx.cumulative_chars_typed as f32 / 5.) / (ctx.cumulative_time_taken as f32 / 60000.))
+            as u32
+    };
+
+    ctx.percentage = if ctx.success == 0 {
+        0.
+    } else {
+        100. * ((ctx.success as f32) / ((ctx.success + ctx.failure) as f32))
+    };
+
+    print!(
+        "{}{}{}:{}:{}     WPM:{} {:.2}%   copy:{:?}  page:{:?}  {}", // copy:{:?}  page:{:?}
+        termion::cursor::Save,
+        termion::cursor::Goto(1, 1),
+        cursor.line_number(),
+        cursor.word_number(),
+        copy.last_word_or_default().chars.len(),
+        wpm,
+        ctx.percentage,
+        copy.lines.first().unwrap_or(&Line { words: vec![] }),
+        page.lines.first().unwrap_or(&Line { words: vec![] }),
+        termion::cursor::Restore
+    );
+
+    print!(
+        r#"{}"#,
+        termion::cursor::Goto(
+            cursor.char_number(),
+            cursor.line_number() + HEADER - 1 - cursor.window.start_line as u16
+        )
+    );
+    stdout.flush().unwrap();
 }
--- src/model.rs
+++ src/model.rs
@@ -1,5 +1,11 @@
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fs::File;
+use std::io::Write;
+
 pub struct Page {
     pub lines: Vec<Line>,
+    pub save: bool,
 }
 
 pub struct Cursor<'page> {
@@ -7,15 +13,24 @@ pub struct Cursor<'page> {
     current_line: usize,
     current_word: usize,
     current_char: usize,
+    pub window: Window,
 }
 
+pub struct Window {
+    pub start_line: usize,
+    pub size: usize,
+}
+
+const WINDOW_MARGIN: usize = 2;
+
 impl<'page> Cursor<'page> {
-    pub fn new(page: &'page Page) -> Cursor<'page> {
+    pub fn new(page: &'page Page, window: Window) -> Cursor<'page> {
         Cursor {
             page,
             current_line: 0,
             current_word: 0,
             current_char: 0,
+            window,
         }
     }
 
@@ -39,32 +54,26 @@ impl<'page> Cursor<'page> {
         self.current_line().words.get(self.current_word)
     }
 
-    pub fn current_char(&self) -> Option<char> {
-        let mut chars = 0;
-        for word in self.current_line().words.iter() {
-            for ch in word.chars.iter() {
-                if chars == self.current_char {
-                    return Some(*ch);
-                }
-                chars += 1;
-            }
-
-            if chars == self.current_char {
-                return Some(' ');
-            }
-            chars += 1; // for the space
-        }
-        None
+    pub fn current_word_or_default(&self) -> &Word {
+        self.current_word().unwrap_or(EMPTY_WORD)
     }
 
-    pub fn next_char(&mut self) {
+    /// retuns true if the cursor moved to the next line
+    pub fn next_char(&mut self) -> bool {
+        let mut moved_to_next_line = false;
         self.current_char += 1;
         if self.current_char > self.current_line().len() {
             self.current_line += 1;
             self.current_word = 0;
             self.current_char = 0;
+            moved_to_next_line = true;
+
+            if self.current_line >= self.window.size + self.window.start_line - WINDOW_MARGIN {
+                self.window.start_line += 1;
+            }
         }
         self.calc_current_word();
+        moved_to_next_line
     }
 
     pub fn prev_char(&mut self) {
@@ -74,6 +83,10 @@ impl<'page> Cursor<'page> {
             }
             self.current_line = self.current_line.saturating_sub(1);
             self.current_char = self.current_line().len();
+
+            if self.current_line <= self.window.start_line + WINDOW_MARGIN {
+                self.window.start_line = self.window.start_line.saturating_sub(1);
+            }
         } else {
             self.current_char -= 1;
         }
@@ -93,8 +106,10 @@ impl<'page> Cursor<'page> {
     }
 }
 
+static EMPTY_WORD: &Word = &Word { chars: vec![] };
+
 impl Page {
-    pub fn parse(text: &str) -> Self {
+    pub fn parse(text: &str, save: bool) -> Self {
         let lines = text.lines();
         let mut page: Vec<Line> = vec![];
         let mut last_line_was_empty = false;
@@ -114,16 +129,99 @@ impl Page {
             });
             page.push(Line { words: parsed_line });
         }
-        Self { lines: page }
+        Self { lines: page, save }
+    }
+
+    pub(crate) fn last_word(&self) -> Option<&Word> {
+        self.lines.last().and_then(|line| line.words.last())
+    }
+    pub(crate) fn last_word_or_default(&self) -> &Word {
+        self.last_word().unwrap_or(EMPTY_WORD)
+    }
+
+    pub(crate) fn push(&mut self, arg: char, new_line: bool) {
+        if self.lines.is_empty() {
+            let line = Line::default();
+            self.lines.push(line);
+        }
+        let last_line = self.lines.last_mut().unwrap();
+        if last_line.words.is_empty() {
+            last_line.words.push(Word::default());
+        }
+        let last_word = last_line.words.last_mut().unwrap();
+        match arg {
+            ' ' => {
+                if new_line {
+                    let mut line = Line::default();
+                    line.words.push(Word::default());
+                    self.lines.push(line);
+                } else {
+                    let skip = if let Some(last_word) = last_line.words.last() {
+                        last_word.chars.is_empty()
+                    } else {
+                        false
+                    };
+                    if !skip {
+                        last_line.words.push(Word::default());
+                    }
+                }
+            }
+            _ => {
+                last_word.chars.push(arg);
+            }
+        }
+        self.changed();
+    }
+
+    pub fn delete(&mut self) {
+        if self.lines.is_empty() {
+            return;
+        }
+        let last_line = self.lines.last_mut().unwrap();
+        if let Some(last_word) = last_line.words.last_mut() {
+            if last_word.chars.pop().is_none() && last_line.words.pop().is_none() {
+                self.lines.pop();
+            }
+        } else {
+            self.lines.pop();
+        }
+        self.changed();
+    }
+
+    fn changed(&self) {
+        if self.save {
+            File::create("war_of_the_worlds.inputs.txt")
+                .unwrap()
+                .write_all(self.to_string().as_bytes())
+                .unwrap();
+        }
     }
 }
 
+impl Display for Page {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for line in self.lines.iter() {
+            for (word_index, word) in line.words.iter().enumerate() {
+                if word_index != 0 {
+                    write!(f, " ")?;
+                }
+                for ch in word.chars.iter() {
+                    write!(f, "{ch}")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, Default)]
 pub struct Line {
     pub words: Vec<Word>,
 }
 
 impl Line {
-    fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.words
             .iter()
             .fold(0, |acc, word| acc + word.chars.len())
@@ -131,7 +229,7 @@ impl Line {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct Word {
     pub chars: Vec<char>,
 }
@@ -155,9 +253,15 @@ mod test {
 
     #[test]
     fn test_parse() {
-        let page = Page::parse("The quick brown fox jumps over the lazy dog.");
+        let mut page = Page::parse("The quick brown fox jumps over the lazy dog.", false);
 
-        let mut cursor = Cursor::new(&page);
+        let mut cursor = Cursor::new(
+            &page,
+            Window {
+                start_line: 0,
+                size: 1,
+            },
+        );
 
         assert_eq!(cursor.current_line().words.len(), 9);
         assert_eq!(cursor.current_word().unwrap().chars.len(), 3);
@@ -171,5 +275,22 @@ mod test {
 
         cursor.prev_char();
         assert_eq!(cursor.current_word().unwrap().chars.len(), 3);
+
+        assert_eq!(
+            page.to_string(),
+            "The quick brown fox jumps over the lazy dog.\n"
+        );
+        page.push(' ', true);
+        page.push('r', false);
+        assert_eq!(
+            page.to_string(),
+            "The quick brown fox jumps over the lazy dog.\nr\n"
+        );
+        page.push(' ', false);
+        page.push('c', false);
+        assert_eq!(
+            page.to_string(),
+            "The quick brown fox jumps over the lazy dog.\nr c\n"
+        );
     }
 }
